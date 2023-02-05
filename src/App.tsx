5d716cdc2684aad71333a26db75e04c031928ab7
Quentin Gibson <quentingibson94@gmail.com>
2023-02-05 01:48:00 -0500

    add qoute

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,20 @@ function App() {
   const messageElement = <span>CAUTION</span>
   return (
     <main className="App">
-      <div>
-        <div style={{ display: "flex" }}>
-          <h2>Welcome to my side of the web</h2>
+
+      {/* Quote Section Start */}
+      <div style={{ display: "grid", gridTemplate: "80%  20% / 70%  30%", margin: "80px 50px" }}>
+        <div style={{ display: "flex", justifyContent: 'center', alignItems: "center" }}>
+          <span style={{ fontSize: "4em", fontWeight: "bolder", textTransform: "lowercase" }}>We do not fear the unknown. We fear what we think we know about the unknown</span>
         </div>
-        <h1>QUENTIN GIBSON</h1>
-        <Marquee background={{ transform: "rotate(-1deg)", marginBottom: 30 }} span={messageElement} />
-        <Marquee background={{ transform: "rotate(1deg)" }} span={messageElement} />
+        <div style={{ display: "flex", justifyContent: 'center', justifyItems: "center", gridRowStart: "2", gridColumnStart: "2" }}>
+          <span style={{ fontSize: "2.3em", fontWeight: "bolder" }}>- Teal Swan</span>
+        </div>
       </div>
+      {/* Quote Section End */}
+
+      {/* <Marquee background={{ transform: "rotate(-1deg)", marginBottom: 30 }} span={messageElement} /> */}
+      {/* <Marquee background={{ transform: "rotate(1deg)" }} span={messageElement} /> */}
     </main>
 
   )
