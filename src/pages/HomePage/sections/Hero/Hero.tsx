807f56dfb85fc64c9c4dcea1c8c8a3d52518df7b
Quentin Gibson <qgibson3@students.kennesaw.edu>
2023-02-26 16:58:27 -0500

    refactor hero css to module

--- src/pages/HomePage/sections/Hero/Hero.tsx
+++ src/pages/HomePage/sections/Hero/Hero.tsx
@@ -1,93 +1,18 @@
-import './hero.css'
-interface ReactStyles {
-  [key: string]: React.CSSProperties
-}
-const style: ReactStyles = {
-  section: {
-    margin: '0 50px',
-  },
-  heroContainer: {
-    display: "grid",
-    gridTemplateColumns: "1fr 300px",
-    gridGap: '4rem',
-    marginTop: "50px",
-  },
-  heroIntroContainer: {
-    display: "flex",
-    flexDirection: "column"
-  },
-  heroIntro: {
-    letterSpacing: 2,
-    fontSize: "clamp(3em,7em,14em)",
-    margin: 0,
-  },
-  heroBanner: {
-    gridColumnStart: 2,
-    position: "relative",
-    background: "url(./empty-circles.svg) no-repeat center center",
-    display: "flex",
-    alignItems: "center",
-    width: 310,
-    justifySelf: "center",
-    minHeight: 700,
-  },
-  heroImage: {
-    width: "100%",
-    transform: "scale(0.99)",
-    borderRadius: "192px 192px 192px 192px",
-    zIndex: "-100",
-  },
-  heroCircles: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    height: "100%",
-    width: "100%",
-  },
-  heroUnderLine: {
-    position: "relative",
-    width: "60%",
-    top: -70,
-    transform: 'scale(1.5)',
-    left: 100,
-  },
-  heroSmallContainer: {
-    display: "grid",
-    alignItems: "center",
-    gridTemplateColumns: "30% 50%",
-    gridColumnStart: 1,
-    gridRowStart: 2
-  },
-  heroSmallButton: {
-    padding: "10px 20px",
-    background: "white",
-    color: "black",
-    borderRadius: "30px",
-    fontSize: "1.3em",
-    marginRight: "1em"
-  },
-  heroSmallContent: {
-    fontSize: "1em",
-  }
+import styles from './hero.module.css'
 
-}
 const Hero = () => {
   return (
-    <section style={style.section}>
-      <div style={style.heroContainer}>
-        <div style={style.heroIntroContainer} >
-          <h1 style={style.heroIntro}>
-            QUENTIN GIBSON <br /> <span className="blue-accent">DA DOPE DEVELOPER</span>
-            <img style={style.heroUnderLine} src="./crazy-line.svg" alt="" />
+    <section className={styles.section}>
+      <div className={styles.heroContainer}>
+        <div className={styles.heroIntroContainer} >
+          <h1 className={styles.heroIntro}>
+            QUENTIN GIBSON <br /> <span className={ styles.blueAccent }>DA DOPE DEVELOPER</span>
           </h1>
         </div>
-        <div style={style.heroBanner}>
-          <img style={style.heroImage} src="https://picsum.photos/400" alt="A random image" />
+        <div className={styles.heroSmallContainer}>
+          <button className={styles.heroSmallButton}>DOPE PROJETS</button>
         </div>
-        <div style={style.heroSmallContainer}>
-          <button style={style.heroSmallButton}>SEE MORE</button>
-          <p style={style.heroSmallContent}>Come check out my collection of dope projects. All made with React and React Native.</p>
-        </div>
+        <h2>Shoutout to all my Afican American Developers</h2>
       </div>
     </section>
   )
